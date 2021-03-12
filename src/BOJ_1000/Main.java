c27968822c29f480f35f8417084de26b572cac6d
jm choi <527wlals@gmail.com>
2021-03-13 04:33:27 +0900

    add problem 1000

---
+++ src/BOJ_1000/Main.java
@@ -0,0 +1,14 @@
+package BOJ_1000;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);    // System.in 입력한 값을 바이트 단위로 읽기
+        int A, B;
+        A = sc.nextInt();
+        B = sc.nextInt();
+
+        System.out.println(A + B);
+    }
+}
