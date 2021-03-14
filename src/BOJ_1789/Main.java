728da0c9f4d1c364a326b3f497d8eba6dcf10e72
jm choi <527wlals@gmail.com>
2021-03-14 19:17:55 +0900

    add problem 1789

---
+++ src/BOJ_1789/Main.java
@@ -0,0 +1,20 @@
+package BOJ_1789;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long S = sc.nextLong();
+        int i = 0;
+
+        while(S > 0) {
+            if(S - (i+1) < 0) {
+                break;
+            }
+            i++;
+            S -= i;
+        }
+        System.out.println(i);
+    }
+}
