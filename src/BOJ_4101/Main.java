648202b03307de338a21b688b12c7d2245aaac05
jm choi <527wlals@gmail.com>
2021-03-16 02:16:57 +0900

    add problem 4101

---
+++ src/BOJ_4101/Main.java
@@ -0,0 +1,22 @@
+package BOJ_4101;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(true) {
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            if(A == 0 && B == 0) {
+                break;
+            }
+            if(A > B) {
+                System.out.println("Yes");
+            }
+            else {
+                System.out.println("No");
+            }
+        }
+    }
+}
