904f1bda566e2b1302dbaa30d8732b21fe8d718a
jm choi <527wlals@gmail.com>
2021-03-21 07:02:57 +0900

    add problem 2742

---
+++ src/Solving_Step_by_Step/Chap03/BOJ_2742/Main.java
@@ -0,0 +1,18 @@
+package Solving_Step_by_Step.Chap03.BOJ_2742;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+
+        br.close();
+        for(int i = N; i > 0; i--) {
+            bw.write(i + "\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
