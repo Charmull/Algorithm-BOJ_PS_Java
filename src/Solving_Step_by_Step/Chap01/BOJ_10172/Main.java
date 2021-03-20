015d998f627471a2a8def4617f700efb0cccd96d
jm choi <527wlals@gmail.com>
2021-03-20 17:12:25 +0900

    add problem 10172

---
+++ src/Solving_Step_by_Step/Chap01/BOJ_10172/Main.java
@@ -0,0 +1,7 @@
+package Solving_Step_by_Step.Chap01.BOJ_10172;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("|\\_/|\n|q p|   /}\n( 0 )\"\"\"\\\n|\"^\"`    |\n||_/=\\\\__|");
+    }
+}
