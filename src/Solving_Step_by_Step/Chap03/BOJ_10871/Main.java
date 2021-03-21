938c36fff75d4dd6fa4f25b0fc32ce2c3d24c808
jm choi <527wlals@gmail.com>
2021-03-22 07:51:04 +0900

    add problem 10871

---
+++ src/Solving_Step_by_Step/Chap03/BOJ_10871/Main.java
@@ -0,0 +1,26 @@
+package Solving_Step_by_Step.Chap03.BOJ_10871;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int X = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++) {
+            int temp = Integer.parseInt(st.nextToken());
+            if(temp < X) {
+                bw.write(temp + " ");
+            }
+        }
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
