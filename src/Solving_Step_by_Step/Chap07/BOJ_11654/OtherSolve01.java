4a7ef221142064c280e240b8c9f1825324e7d8f6
jm choi <527wlals@gmail.com>
2021-03-26 19:24:37 +0900

    add problem 11654 OtherSolve01

---
+++ src/Solving_Step_by_Step/Chap07/BOJ_11654/OtherSolve01.java
@@ -0,0 +1,12 @@
+package Solving_Step_by_Step.Chap07.BOJ_11654;
+
+import java.io.IOException;
+
+public class OtherSolve01 {
+    public static void main(String[] args) throws IOException {
+        int num = System.in.read();
+        System.out.println(num);
+    }
+}
+
+// java 입력관련 클래스 공부하기(https://st-lab.tistory.com/41?category=830901)
