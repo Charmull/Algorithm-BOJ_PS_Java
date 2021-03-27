a003039f37df811fb2b011cf2d2a217f7e9ade25
jm choi <527wlals@gmail.com>
2021-03-27 17:27:56 +0900

    add problem 11720

---
+++ src/Solving_Step_by_Step/Chap07/BOJ_11720/Main.java
@@ -0,0 +1,25 @@
+package Solving_Step_by_Step.Chap07.BOJ_11720;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        String input = br.readLine();
+        int sum = 0;
+
+        for(int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(input.substring(i, i+1));
+            sum+= arr[i];
+        }
+
+        bw.write(Integer.toString(sum));
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
