246d14d3b91af0ee003a9e0a8e6b04cab98ba363
jm choi <527wlals@gmail.com>
2021-04-13 12:20:03 +0900

    study more problem 2775

--- src/Solving_Step_by_Step/Chap08/BOJ_2775/Main.java
+++ src/Solving_Step_by_Step/Chap08/BOJ_2775/Main.java
@@ -2,7 +2,12 @@ package Solving_Step_by_Step.Chap08.BOJ_2775;
 
 import java.io.*;
 
+// '1 <= k, n <= int 최대 범위' 까지 실행되도록 코드 작성함.
+// 문제 자체에 대한 답으로는 시간 측면에 있어서 효율적이지 못함. (376ms)
+// 문제에서 k와 n의 크기에 대한 조건이 바뀌어도 수정 필요 없음.
+// TODO: 추후에 더 공부하고 정리해보자.
 public class Main {
+    // k층의 n호에 입주해야하는 인원수를 리턴해주는 함수
     public static int people(int k, int n) {
         int result = 0;
         if(n == 1) {
