ce733e2c1bf1360dabbe56c8240b642e39dbd7e2
jm choi <527wlals@gmail.com>
2021-04-20 11:30:14 +0900

    add problem 10757

---
+++ src/Solving_Step_by_Step/Chap08/BOJ_10757/Main.java
@@ -0,0 +1,21 @@
+package Solving_Step_by_Step.Chap08.BOJ_10757;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+// BigInteger 사용
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        BigInteger A = new BigInteger(st.nextToken());
+        BigInteger B = new BigInteger(st.nextToken());
+
+        System.out.println(A.add(B));
+
+        br.close();
+    }
+}
