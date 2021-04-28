f32d5f111e54c0217c85d3d4a2abcdf668a9a76c
jm choi <527wlals@gmail.com>
2021-04-28 21:38:28 +0900

    study more problem 1011

--- src/Solving_Step_by_Step/Chap08/BOJ_1011/OtherSolve01.java
+++ src/Solving_Step_by_Step/Chap08/BOJ_1011/OtherSolve01.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+// 2중 반복문을 사용하지 않고, 규칙을 찾아 해결하는 방법
 public class OtherSolve01 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -19,14 +20,15 @@ public class OtherSolve01 {
             int distance = y - x;
 
             int max = (int)Math.sqrt(distance);     // max는 distance를 이동할때의 한번에 이동하는 가장 큰 수
+                                                    // max는 distance의 제곱근 (int형)
 
-            if(max == Math.sqrt(distance)) {
+            if(max == Math.sqrt(distance)) {        // Math.sqrt(distance)가 max와 같으면 (이 때는 max값이 변할 때와 같다)
                 sb.append(max*2 - 1).append("\n");
             }
-            else if(distance <= max*max + max) {
+            else if(distance <= max*max + max) {    // max*max < distance <= max*max + max 일때
                 sb.append(max*2).append("\n");
             }
-            else {
+            else {                                  // max*max + max < distance <= (max+1)*(max+1) - 1
                 sb.append(max*2 + 1).append("\n");
             }
         }
