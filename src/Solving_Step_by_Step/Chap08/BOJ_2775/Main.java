d708de4b72cc386c4bf5f7e7a06564f7617d77aa
jm choi <527wlals@gmail.com>
2021-04-29 18:52:53 +0900

    study more problem 2775

--- src/Solving_Step_by_Step/Chap08/BOJ_2775/Main.java
+++ src/Solving_Step_by_Step/Chap08/BOJ_2775/Main.java
@@ -6,8 +6,11 @@ import java.io.*;
 // 문제 자체에 대한 답으로는 시간 측면에 있어서 효율적이지 못함. (376ms)
 // 문제에서 k와 n의 크기에 대한 조건이 바뀌어도 수정 필요 없음.
 // TODO: 추후에 더 공부하고 정리해보자.
+    // 결국, OtherSolve01이 문제 자체에 대한 풀이로써 시간 측면에서 더 효율적이라는 장점이 있지만
+    // Main은 int 범위를 벗어나지 않는 한, k와 n의 크기가 바뀌어도 코드 수정이 필요없다는 장점이 있음.
 public class Main {
     // k층의 n호에 입주해야하는 인원수를 리턴해주는 함수
+    // 적절히 분기시키고 재귀를 사용
     public static int people(int k, int n) {
         int result = 0;
         if(n == 1) {
@@ -37,6 +40,8 @@ public class Main {
             int n = Integer.parseInt(br.readLine());
             int result = 0;
 
+            // people 함수를 사용할 것이기때문에, 굳이 if-else문으로 result 값을 분기시킬 필요 없음. (people 함수에서 분기하고 있음)
+            // OtherSolve02 참고
             if(k == 0) {
                 result = n;
             }
