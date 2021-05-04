8d71b13c2311879db43987a48aa96e1bc6349380
jm choi <527wlals@gmail.com>
2021-05-04 14:16:18 +0900

    add problem 1978

---
+++ src/Solving_Step_by_Step/Chap09/BOJ_1978/Main.java
@@ -0,0 +1,43 @@
+package Solving_Step_by_Step.Chap09.BOJ_1978;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static boolean isPrime(int num) {
+        if(num == 1) {
+            return false;
+        }
+        else if(num == 2) {
+            return true;
+        }
+        else {
+            for(int i = 2; i < num; i++) {
+                if(num % i == 0) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int count = 0;
+        for(int i = 0; i < N; i++) {
+            int temp = Integer.parseInt(st.nextToken());
+            if(isPrime(temp)) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+        br.close();
+    }
+}
