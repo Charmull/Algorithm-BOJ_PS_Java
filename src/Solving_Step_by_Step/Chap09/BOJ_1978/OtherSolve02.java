e106ae997e89da437b3779604f93efa93850d984
jm choi <527wlals@gmail.com>
2021-05-06 20:20:14 +0900

    add problem 1978 OtherSolve02

---
+++ src/Solving_Step_by_Step/Chap09/BOJ_1978/OtherSolve02.java
@@ -0,0 +1,42 @@
+package Solving_Step_by_Step.Chap09.BOJ_1978;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class OtherSolve02 {
+    public static boolean[] makePrime(int max) {
+        boolean[] isPrime = new boolean[max + 1];
+        isPrime[0] = true;
+        isPrime[1] = true;
+
+        for(int i = 2; i <= Math.sqrt(max); i++) {
+            if(isPrime[i] == false) {
+                for(int j = i*i; j < max + 1; j = j+i) {
+                    isPrime[j] = true;
+                }
+            }
+        }
+
+        return isPrime;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        boolean[] primeArr = makePrime(1000);
+        int count = 0;
+        for(int i = 0; i < N; i++) {
+            int temp = Integer.parseInt(st.nextToken());
+            if(primeArr[temp] == false) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+        br.close();
+    }
+}
