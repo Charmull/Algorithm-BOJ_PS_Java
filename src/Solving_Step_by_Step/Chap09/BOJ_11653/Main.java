2f239a56aec0db9281a8f1332265cb5d1905ce31
jm choi <527wlals@gmail.com>
2021-05-19 21:29:25 +0900

    study more problem 11653 and 'compare the memory of Main and OtherSolve'

--- src/Solving_Step_by_Step/Chap09/BOJ_11653/Main.java
+++ src/Solving_Step_by_Step/Chap09/BOJ_11653/Main.java
@@ -4,25 +4,29 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+// 메모리 : 14692KB, 시간 : 168ms
 public class Main {
-    public static void returnPrime(int num) {
-        int temp = 2;
+    public static void returnPrime(int num, StringBuilder sb) {
+        int i = 2;
         while(num != 1) {
-            if(num % temp == 0) {
-                System.out.println(temp);
-                num /= temp;
+            if(num % i == 0) {
+                sb.append(i).append('\n');
+                num /= i;
             }
             else {
-                temp++;
+                i++;
             }
         }
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         int N = Integer.parseInt(br.readLine());
 
-        returnPrime(N);
+        returnPrime(N, sb);
+        System.out.println(sb);
+
         br.close();
     }
 }
