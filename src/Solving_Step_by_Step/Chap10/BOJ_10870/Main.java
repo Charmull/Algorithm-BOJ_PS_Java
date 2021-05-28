46cfec12c756ea4699726ea74858115afba4b636
jm choi <527wlals@gmail.com>
2021-05-28 19:17:26 +0900

    add problem 10870

---
+++ src/Solving_Step_by_Step/Chap10/BOJ_10870/Main.java
@@ -0,0 +1,27 @@
+package Solving_Step_by_Step.Chap10.BOJ_10870;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    private static int fibonacci(int n) {
+        if(n == 0) {
+            return 0;
+        }
+        else if(n == 1) {
+            return 1;
+        }
+        else {
+            return fibonacci(n-1) + fibonacci(n-2);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(fibonacci(n));
+
+        br.close();
+    }
+}
