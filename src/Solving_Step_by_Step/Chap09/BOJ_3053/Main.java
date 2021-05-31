d5a16e02c74bd661281912dff5b866fadf3bcd90
jm choi <527wlals@gmail.com>
2021-05-31 15:04:52 +0900

    study more problem 3053 and 'compare the memory and time'

--- src/Solving_Step_by_Step/Chap09/BOJ_3053/Main.java
+++ src/Solving_Step_by_Step/Chap09/BOJ_3053/Main.java
@@ -15,3 +15,15 @@ public class Main {
         br.close();
     }
 }
+
+// System.out.println(R*R*Math.PI + "\n" + R*R*2.0) 의 경우,
+//  print 전에 연산이 필요해서인지 시간과 메모리가 더 많이 요구된다. (16276KB, 160ms)
+
+// int R = Integer.parseInt(br.readLine()) 의 경우,
+//  후에 int형과 double형의 연산에서 int형을 double형으로 바꾼 후 계산을 하기때문인지 더 많은 시간과 메모리가 요구된다. (14220KB, 132ms)
+
+// System.out.println(R*R*2) 의 경우,
+//  double형 2개와 int형 1개를 연산하는 것이기때문에, int형을 double형으로 바꾼 후 계산을 해서인지 더 많은 메모리가 요구된다.
+//  아마 시간도 약간 더 걸릴 것으로 예상된다. (14188KB, 124ms)
+
+// 현재 코드는 14172KB, 124ms
