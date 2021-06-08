0bd3f93266eb40b087cc44fa5b0ef81a768f4c78
jm choi <527wlals@gmail.com>
2021-06-08 15:12:49 +0900

    study more problem 2581

--- src/Solving_Step_by_Step/Chap09/BOJ_2581/OtherSolve01.java
+++ src/Solving_Step_by_Step/Chap09/BOJ_2581/OtherSolve01.java
@@ -14,7 +14,7 @@ public class OtherSolve01 {
 
         for(int i = 2; i <= Math.sqrt(max); i++) {
             if(isPrime[i]) {
-                for(int j = i*2; j < max + 1; j += i) {
+                for(int j = i*i; j < max + 1; j += i) {
                     isPrime[j] = false;
                 }
             }
