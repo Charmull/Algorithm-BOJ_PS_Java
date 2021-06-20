761e0f501b48de797e308aa8294d24eb000d448f
jm choi <527wlals@gmail.com>
2021-06-20 19:12:46 +0900

    add problem 10989 OtherSolve01

---
+++ src/Solving_Step_by_Step/Chap12/BOJ_10989/OtherSolve01.java
@@ -0,0 +1,28 @@
+package Solving_Step_by_Step.Chap12.BOJ_10989;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class OtherSolve01 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[10000];
+        for(int i = 0; i < n; i++) {
+            int temp = Integer.parseInt(br.readLine());
+            arr[temp - 1]++;
+        }
+
+        for(int i = 0; i < 10000; i++) {
+            if(arr[i] > 0) {
+                sb.append((i+1 + "\n").repeat(arr[i]));
+            }
+        }
+
+        System.out.println(sb);
+        br.close();
+    }
+}
