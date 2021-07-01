effe1018080629abcf49e6ff411d4e160b859313
jm choi <527wlals@gmail.com>
2021-07-01 20:29:51 +0900

    add problem 2750 OtherSolve03

---
+++ src/Solving_Step_by_Step/Chap12/BOJ_2750/OtherSolve03.java
@@ -0,0 +1,40 @@
+package Solving_Step_by_Step.Chap12.BOJ_2750;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 버블정렬
+public class OtherSolve03 {
+    private static void sort(int[] arr, int len) {
+        for(int i = 0; i < len; i++) {
+            for(int j = 0; j < len-1-i; j++) {
+                if(arr[j] > arr[j+1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        sort(arr, n);
+
+        for(int num : arr) {
+            sb.append(num).append('\n');
+        }
+
+        System.out.println(sb);
+        br.close();
+    }
+}
