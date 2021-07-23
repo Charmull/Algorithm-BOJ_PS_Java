640370662ff8ed1d1ebbe9ce2e3794f10adc5659
jm choi <527wlals@gmail.com>
2021-07-23 19:38:15 +0900

    add problem 1003

---
+++ src/Solving_Step_by_Step/Chap15/BOJ_1003/Main.java
@@ -0,0 +1,38 @@
+package Solving_Step_by_Step.Chap15.BOJ_1003;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    static int zero;
+    static int one;
+    static int zero_plus_one;
+
+    private static void fibonacci(int N) {
+        zero = 1;
+        one = 0;
+        zero_plus_one = 1;
+
+        for (int i = 0; i < N; i++) {
+            zero = one;
+            one = zero_plus_one;
+            zero_plus_one = zero + one;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+            int N = Integer.parseInt(br.readLine());
+            fibonacci(N);
+            sb.append(zero).append(' ').append(one).append('\n');
+        }
+
+        System.out.println(sb);
+        br.close();
+    }
+}
