1f49f82636445bcbb7a25e0499dc8404aad0036c
jm choi <527wlals@gmail.com>
2021-09-15 19:36:30 +0900

    study more problem 9663

--- src/Solving_Step_by_Step/Chap14/BOJ_9663/Main.java
+++ src/Solving_Step_by_Step/Chap14/BOJ_9663/Main.java
@@ -9,12 +9,13 @@ public class Main {
     public static int N;
     public static int count = 0;
 
+    // 해당 열에서 col번째 행에 놓을 수 있는가
     private static boolean Possibility(int col) {
         for (int i = 0; i < col; i++) {
-            if (arr[col] == arr[i]) {
+            if (arr[col] == arr[i]) {       // 이미 같은 행에 존재할 경우
                 return false;
             }
-            else if (Math.abs(col - i) == Math.abs(arr[col] - arr[i])) {
+            else if (Math.abs(col - i) == Math.abs(arr[col] - arr[i])) {        // 이미 대각선에 존재할 경우
                 return false;
             }
         }
@@ -22,7 +23,7 @@ public class Main {
     }
 
     private static void nQueen(int depth) {
-        if (depth == N) {
+        if (depth == N) {           // 행을 다 채웠을 때 count 1 증가시키고 리턴
             count++;
             return;
         }
