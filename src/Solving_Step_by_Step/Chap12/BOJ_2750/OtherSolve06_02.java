0b0cc7c39a896221ecb5bd4e840781a7e086d130
jm choi <527wlals@gmail.com>
2021-09-27 22:48:26 +0900

    add problem 2750 OtherSolve06_02

---
+++ src/Solving_Step_by_Step/Chap12/BOJ_2750/OtherSolve06_02.java
@@ -0,0 +1,68 @@
+package Solving_Step_by_Step.Chap12.BOJ_2750;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class OtherSolve06_02 {
+    private static void merge(int[] arr, int start, int mid, int end) {
+        int i = start;
+        int j = mid + 1;
+        int k = 0;
+        int[] temp = new int[end - start + 1];
+
+        while(i <= mid && j <= end) {
+            if(arr[i] <= arr[j]) {
+                temp[k++] = arr[i++];
+            }
+            else {
+                temp[k++] = arr[j++];
+            }
+        }
+
+        if(i > mid) {
+            while(j <= end) {
+                temp[k++] = arr[j++];
+            }
+        }
+        else {
+            while(i <= mid) {
+                temp[k++] = arr[i++];
+            }
+        }
+
+        k = 0;
+        for(int l = start; l <= end; l++) {
+            arr[l] = temp[k++];
+        }
+    }
+
+    private static void mergeSort(int[] arr, int start, int end) {
+        if(start != end) {
+            int mid = (start + end) / 2;
+            mergeSort(arr, start, mid);
+            mergeSort(arr, mid + 1, end);
+            merge(arr, start, mid, end);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        mergeSort(arr, 0, n - 1);
+
+        for(int num : arr) {
+            sb.append(num).append('\n');
+        }
+
+        System.out.println(sb);
+        br.close();
+    }
+}
