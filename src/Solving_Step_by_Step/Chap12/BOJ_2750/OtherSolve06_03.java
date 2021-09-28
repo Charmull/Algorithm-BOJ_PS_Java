b60af0ef21fcee0e2d7d3a122da5d3a74b28c2dc
jm choi <527wlals@gmail.com>
2021-09-28 19:42:39 +0900

    add problem 2750 OtherSolve06_03

---
+++ src/Solving_Step_by_Step/Chap12/BOJ_2750/OtherSolve06_03.java
@@ -0,0 +1,71 @@
+package Solving_Step_by_Step.Chap12.BOJ_2750;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class OtherSolve06_03 {
+    private static void merge(int[] arr, int[] leftArr, int[] rightArr, int mid, int len) {
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        while(i < mid && j < len - mid) {
+            if(leftArr[i] < rightArr[j]) {
+                arr[k++] = leftArr[i++];
+            }
+            else {
+                arr[k++] = rightArr[j++];
+            }
+        }
+
+        if(i == mid) {
+            while(j < len - mid) {
+                arr[k++] = rightArr[j++];
+            }
+        }
+        else {
+            while(i < mid) {
+                arr[k++] = leftArr[i++];
+            }
+        }
+    }
+    private static void mergeSort(int[] arr, int len) {
+        if(len != 1) {
+            int mid = len / 2;
+            int[] leftArr = new int[mid];
+            int[] rightArr = new int[len - mid];
+
+            for(int i = 0; i < mid; i++) {
+                leftArr[i] = arr[i];
+            }
+            for(int i = 0; i < len - mid; i++) {
+                rightArr[i] = arr[mid + i];
+            }
+
+            mergeSort(leftArr, mid);
+            mergeSort(rightArr, len - mid);
+            merge(arr, leftArr, rightArr, mid, len);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        mergeSort(arr, n);
+
+        for(int num : arr) {
+            sb.append(num).append('\n');
+        }
+
+        System.out.println(sb);
+        br.close();
+    }
+}
